cc09570e20da46b09b8dc27a6b82e9a3603ca29f
hamidafghan <abd.hamidaf@gmail.com>
2023-07-06 11:35:14 +0200

    Add the footer and header in other dir

---
+++ app/js/app.js
@@ -0,0 +1,15 @@
+// Load the header
+fetch("app/layout/header.html")
+  .then((response) => response.text())
+  .then((content) => {
+    document.getElementById("header").innerHTML = content;
+  })
+  .catch((error) => console.log(error));
+
+// load the footer
+fetch("app/layout/footer.html")
+  .then((response) => response.text())
+  .then((content) => {
+    document.getElementById("footer").innerHTML = content;
+  })
+  .catch((error) => console.log(error));
